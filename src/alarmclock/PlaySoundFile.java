015466a38342746a9d568c7cd4157720c8a772cb
Mark Grant <m.grant.prg@gmail.com>
2012-05-10 13:55:07 +0100

    Commit following finalisation of PlaySoundFile and PlaySoundThread classes including JavaDoc.

--- src/alarmclock/PlaySoundFile.java
+++ src/alarmclock/PlaySoundFile.java
@@ -8,36 +8,60 @@ import java.net.URL;
 import java.applet.*;
 
 /**
- *
+ * Class to play a sound file using the AudioClip player. Supports the playing
+ * of JAR embedded files and of external files. Also supports play once or loop.
  * @author mgrantprg
  */
 public class PlaySoundFile {
 
     private AudioClip player;
 
+    /**
+     * Plays the 'file' embedded in the JAR.
+     * Can be stopped using the stopPlayingFile Method.
+     * @param file Name of embedded file to play.
+     */
     public void playFile(String file) {
         URL fileToPlay = this.getClass().getResource(file);
         player = Applet.newAudioClip(fileToPlay);
         player.play();
     }
 
+    /**
+     * Plays the file as detailed in the URL.
+     * Can be stopped using the stopPlayingFile Method.
+     * @param file URL of the path to the file to play.
+     */
     public void playFile(URL file) {
         player = Applet.newAudioClip(file);
         player.play();
     }
 
+    /**
+     * Plays the 'file' embedded in the JAR in a continuous loop.
+     * Can be stopped using the stopPlayingFile Method.
+     * @param file Name of the embedded file to play.
+     */
     public void playFileLoop(String file) {
         URL fileToPlay = this.getClass().getResource(file);
         player = Applet.newAudioClip(fileToPlay);
         player.loop();
     }
 
+    /**
+     * Plays the file as detailed in the URL in a continuous loop.
+     * Can be stopped using the stopPlayingFile Method.
+     * @param file URL of the path to the file to play.
+     */
     public void playFileLoop(URL file) {
         player = Applet.newAudioClip(file);
         player.loop();
     }
 
-    public void stopFile() {
+    /**
+     * Stops playing the current file.
+     */
+    public void stopPlayingFile() {
         player.stop();
     }
 }
--- src/alarmclock/PlaySoundThread.java
+++ src/alarmclock/PlaySoundThread.java
@@ -7,7 +7,9 @@ package alarmclock;
 import java.net.URL;
 
 /**
- *
+ * Runnable Class to play a sound file in a separate thread. Utilises
+ * PlaySoundFile class to support play and loop of sound files embedded in the
+ * JAR or external files.
  * @author mgrantprg
  */
 public class PlaySoundThread implements Runnable {
@@ -17,18 +19,33 @@ public class PlaySoundThread implements Runnable {
     private boolean playFile;
     private boolean loop;
 
+    /**
+     * Public constructor used to build an object that plays a file embedded
+     * in the JAR.
+     * @param file Name of JAR embedded file to play.
+     * @param loop True means play in a continuous loop, false means play once.
+     */
     public PlaySoundThread(String file, boolean loop) {
         this.fileToPlay = file;
         this.playFile = true;
         this.loop = loop;
     }
 
+    /**
+     * Public constructor used to build an object that plays an external file
+     * pointed at by an URL.
+     * @param fileToPlayURL Path to the external file to play.
+     * @param loop True means play in a continuous loop, false means play once.
+     */
     public  PlaySoundThread(URL fileToPlayURL, boolean  loop) {
         this.fileToPlayURL = fileToPlayURL;
         this.playFile = false;
         this.loop = loop;
     }
 
+    /**
+     * Override of Thread run() method.
+     */
     public void run() {
         Thread thisThread = Thread.currentThread();
         PlaySoundFile player = new PlaySoundFile();
@@ -43,16 +60,17 @@ public class PlaySoundThread implements Runnable {
                 player.playFileLoop(fileToPlayURL);
             else
                 player.playFile(fileToPlayURL);
-
+        
+        // This loop allows for the thread to be interrupted.
         while (!Thread.interrupted()) {
             try {
                 thisThread.sleep(20000);
             }
             catch (InterruptedException e) {
-                player.stopFile();
+                player.stopPlayingFile();
                 return;
             }
         }
-        player.stopFile();
+        player.stopPlayingFile();
     }
 }
